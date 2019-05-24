27a30be9172fb52253f636394164f91185865de5
xfarsx <fernando.arsantos@gmail.com>
2019-05-24 02:15:12 -0300

    layout login - atualização 1.1

--- app/src/main/java/br/com/digitalhouse/staruniverse/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/staruniverse/MainActivity.java
@@ -1,13 +1,34 @@
 package br.com.digitalhouse.staruniverse;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton buttonFace;
+    private ImageButton buttonGmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_layout);
+
+      buttonFace = findViewById(R.id.btnFace);
+
+        buttonFace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                setContentView(R.layout.facebook_layout);
+
+            }
+        });
+
     }
+
+
 }
