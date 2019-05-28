366c845c71f9b4ef61dc60842e1d7899d1c91238
xfarsx <fernando.arsantos@gmail.com>
2019-05-28 20:33:05 -0300

    layout login - atualização 1.5 (Tela de Cadastro)

--- app/src/main/java/br/com/digitalhouse/staruniverse/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/staruniverse/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton buttonGmail;
     private Button buttonCadastrese;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
