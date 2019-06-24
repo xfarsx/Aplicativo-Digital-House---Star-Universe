5f94b4f3b1b2643d6166c1a5c5b4dee9c3698259
marcelo0468 <marcelo0468@outlook.com>
2019-06-23 23:43:20 -0300

    Merge branch 'master' of https://github.com/xfarsx/StarUniverseApp into marcelo
    # Conflicts:
    #	build.gradle
    
    Implementação de funçoes do menu, troca de senha e troca de e-mail

--- app/src/main/java/br/com/digitalhouse/staruniverse/cadastro/PerfilActivity.java
+++ app/src/main/java/br/com/digitalhouse/staruniverse/cadastro/PerfilActivity.java
@@ -1,14 +1,20 @@
 package br.com.digitalhouse.staruniverse.cadastro;
 
+
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import br.com.digitalhouse.staruniverse.R;
 import br.com.digitalhouse.staruniverse.home.HomeActivity;
+import br.com.digitalhouse.staruniverse.login.LoginActivity;
 
 public class PerfilActivity extends AppCompatActivity {
 
@@ -24,11 +30,14 @@ public class PerfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
+        setUpToolbar();
+
         alteraEmail = findViewById(R.id.btn_alteraremail);
         alteraEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(PerfilActivity.this, "Altera e-mail", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(PerfilActivity.this, TrocaEmailActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -58,14 +67,33 @@ public class PerfilActivity extends AppCompatActivity {
         });
 
         sair = findViewById(R.id.btn_sair);
-        alteraSenha.setOnClickListener(new View.OnClickListener() {
+        sair.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(PerfilActivity.this, HomeActivity.class);
+                Intent intent = new Intent(PerfilActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
 
     }
 
+    protected void setUpToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if(toolbar != null){
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //Botão adicional na ToolBar
+        switch (item.getItemId()) {
+            case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
+                startActivity(new Intent(this, HomeActivity.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
+                break;
+            default:break;
+        }
+        return true;
+    }
 }
